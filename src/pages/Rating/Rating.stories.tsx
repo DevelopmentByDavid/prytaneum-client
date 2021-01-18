313756c0487fd5a66968fafcbf97a7bade564d72
Kelton <kadey001@ucr.edu>
2021-01-18 14:19:56 -0800

    Added providers to RatingPage story

--- src/pages/Rating/Rating.stories.tsx
+++ src/pages/Rating/Rating.stories.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 
 import Main from 'layout/Main';
+import UserProvider from 'contexts/User';
+import TownhallProvider from 'contexts/Townhall';
+import { makeTownhall } from 'prytaneum-typings';
+
 import Component from '.';
 
-export default { title: 'Pages/Rating' };
+export default { title: 'Pages/Rating', component: Component };
+
 
 export function Basic() {
+    const townhall = makeTownhall();
     return (
         <Main>
-            <Component />
+            <UserProvider>
+                <TownhallProvider value={townhall} townhallId='123'>
+                    <Component />
+                </TownhallProvider>
+            </UserProvider>
         </Main>
     );
 }
